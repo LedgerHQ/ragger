54914149800ff1568d1beb14aacfa50ddb5ea847
Xavier Chapron <xavier.chapron@ledger.fr>
2023-01-04 18:23:41 +0100

    [release] v1.2.0

--- src/ragger/__init__.py
+++ src/ragger/__init__.py
@@ -13,7 +13,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 """
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 
 from ragger.logger import init_loggers
 
