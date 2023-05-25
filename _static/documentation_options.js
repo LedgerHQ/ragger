a6a13931aad4ebc81e8a97e5a16f9887aa09e32f
lpascal-ledger <lpascal-ledger@users.noreply.github.com>
2023-05-25 15:25:19 +0000

    deploy: 996835a35b7304b4458010f1a05c8fed83dad081

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.7.3',
+    VERSION: '1.7.4.dev16+g996835a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
