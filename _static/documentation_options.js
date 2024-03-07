d87c983e32bda608616787f06c57287d36eb3ac9
sgliner-ledger <sgliner-ledger@users.noreply.github.com>
2024-03-07 16:45:30 +0000

    deploy: 3fa921124f38f3b2f2e9d26b28b8fd801a56ee93

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.14.5.dev5',
+    VERSION: '1.14.5.dev7',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
