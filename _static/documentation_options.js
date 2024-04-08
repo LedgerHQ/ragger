5093732bcd5dbbb7df419e372f02d6347da2b173
fbeutin-ledger <fbeutin-ledger@users.noreply.github.com>
2024-04-08 16:59:08 +0000

    deploy: e50790d3342670224858ee788980ec5675ad8e5e

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.16.2',
+    VERSION: '1.16.3.dev2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
