309692ed94dad5878c25d186c2056ee108b43ddf
yogh333 <yogh333@users.noreply.github.com>
2025-06-05 08:10:05 +0000

    deploy: 4ceede3b5588c81fdc8f48773277b62a203026c4

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.35.1.dev4',
+    VERSION: '1.35.1.dev7',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
