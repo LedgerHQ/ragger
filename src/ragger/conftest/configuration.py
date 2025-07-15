db9322932e59416d3dddb163c589defb6d5376d6
Francois Beutin <francois.beutin@ledger.fr>
2025-07-15 13:39:07 +0200

    Unify lib loading

--- src/ragger/conftest/configuration.py
+++ src/ragger/conftest/configuration.py
@@ -7,37 +7,39 @@ class OptionalOptions:
     APP_NAME: str
     MAIN_APP_DIR: Optional[str]
     SIDELOADED_APPS: dict
-    SIDELOADED_APPS_DIR: str
+    SIDELOADED_APPS_DIR: Optional[str]
     BACKEND_SCOPE: str
     CUSTOM_SEED: str
     ALLOWED_SETUPS: List[str]
 
 
 OPTIONAL = OptionalOptions(
-    # Use this parameter if you want to make sure the right app is started upon backend instantiation.
-    # This is only used for LedgerWallet and LedgerComm backend.
+    # Use this parameter if you want physical Ragger backends (LedgerWallet and LedgerComm) to start
+    # your application from the Dashboard at test start.
     APP_NAME=str(),
 
-    # If not None, the app being tested with Ragger should be loaded as a library and not as a standalone
-    # app. This parameter points to the repository holding the "main app", i.e the app started by the
-    # backend, which will then use the "local app" as a library.
-    # Use cases : your app is an Ethereum plugin, which will be used as a library by the Ethereum app.
-    # MAIN_APP_DIR contains the directory where the Ethereum app binaries are stored.
-    # example: "./tests/"
+    # If not None, the application being tested with Ragger should be loaded as a library and not as
+    # a standalone application. This parameter points to the repository holding the "main app", i.e
+    # the application started from the Dashboard, which will then use the "local app" as a library.
+    # Example use case: your application is an Ethereum plugin, which will be used as a library by
+    # the Ethereum application.
+    # MAIN_APP_DIR is the directory where the Ethereum application is cloned.
+    #
+    # example: configuration.OPTIONAL.MAIN_APP_DIR = "tests/.test_dependencies/"
+    # Speculos will then start "tests/.test_dependencies/ethereum/build/<device>/bin/app.elf"
+    # There must be exactly one application cloned inside this directory.
     MAIN_APP_DIR=None,
 
-    # use this parameter if you want Speculos to sideload libraries being installed on the device
-    # Useful for Ethereum main app and exchange app, for example
-    # example: {"bitcoin": "Bitcoin", "ethereum": "Ethereum"}
-    # this would result in Speculos being launched with -lbitcoin:/path/to/bitcoin_device.elf
-    # Mutually exclusive with LOAD_MAIN_APP_AS_LIBRARY
+    # Deprecated
     SIDELOADED_APPS=dict(),
 
-    # Mandatory in case SIDELOADED_APPS is used
-    # Relative path to the directory that will store your libs, from the top repository of your application
-    # example: "test/lib_binaries/"
-    # place your compiled application inside with naming "name_device.elf"
-    SIDELOADED_APPS_DIR=str(),
+    # Relative path to the directory that will store library applications needed for the test.
+    # They will be sideloaded by Speculos and can then be called by the main application. This
+    # emulates the applications being installed on the device alongside the main application.
+    #
+    # example: configuration.OPTIONAL.SIDELOADED_APPS_DIR = "tests/.test_dependencies/libraries"
+    # Speculos will then sideload "tests/.test_dependencies/libraries/*/build/<device>/bin/app.elf"
+    SIDELOADED_APPS_DIR=None,
 
     # As the backend instantiation may take some time, Ragger supports multiple backend scopes.
     # You can choose to share the backend instance between {session / module / class / function}
@@ -48,8 +50,10 @@ OPTIONAL = OptionalOptions(
     # Use this parameter if you want speculos to use a custom seed instead of the default one.
     # This would result in speculos being launched with --seed <CUSTOM_SEED>
     # If a seed is provided through the "--seed" pytest command line option, it will override this one.
+    # /!\ DO NOT USE SEEDS WITH REAL FUNDS /!\
     CUSTOM_SEED=str(),
 
+    # /!\ DEPRECATED /!\
     # Use this parameter if you want ragger to handle running different test suites depending on setup
     # Useful when some tests need certain build options and other tests need other build options, or a
     # different Speculos command line
@@ -59,5 +63,6 @@ OPTIONAL = OptionalOptions(
     #
     # "default" setup is always allowed, all tests without explicit decoration depend on default
     # and the --setup option defaults to "default"
+    # /!\ DEPRECATED /!\
     ALLOWED_SETUPS=["default"],
 )
