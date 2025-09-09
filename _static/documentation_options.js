a9150a91da200a80bb88e8749b2eec59ec89e4ca
cedelavergne-ledger <cedelavergne-ledger@users.noreply.github.com>
2025-09-09 08:06:44 +0000

    deploy: 178742d515be912fef07a49fbd3102d2f43d5505

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.40.1',
+    VERSION: '1.40.2.dev3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
